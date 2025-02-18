36f8c7ecc01568e0867b51aa2079208ff80c24dc
Muhammad Taimoor <mtaimoorjamali82@gmail.com>
2025-02-18 17:50:52 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Password-hide-show
- This mini-project is built using Vite and Tailwind CSS to create a password input with a visibility toggle. It also includes a real-time password strength indicator. Based on the length, it displays 'Weak' , 'Medium' , or 'Strong'.
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [pshow, setPshow] = useState(false);
+  const [pcheck, setPcheck] = useState('');
+  const [password, setPassword] = useState('');
+
+  const passwordShow = (e) => {
+    const passValue = e.target.value
+    setPassword(passValue);
+
+    if (passValue.length === 0) {
+      setPcheck('');
+    }
+
+    else if (passValue.length < 4) {
+      setPcheck('week');
+    }
+
+    else if (passValue.length < 8) {
+      setPcheck('medium');
+    }
+
+    else {
+      setPcheck('strong');
+    }
+  }
+
+  const getBorderColor = () => {
+    if (pcheck === 'week') return 'border-red-500';
+    if (pcheck === 'medium') return 'border-yellow-500';
+    if (pcheck === 'strong') return 'border-green-500';
+    return 'border-gray-500';
+  }
+
+  const getTextColor = () => {
+    if (pcheck === 'week') return 'text-red-500';
+    if (pcheck === 'medium') return 'text-yellow-500';
+    if (pcheck === 'strong') return 'text-green-500';
+    return 'text-white';
+  }
+
+
+  return (
+    <>
+      <div className="containe w-[100vw] h-[100vh] m-auto bg-gray-900 text-white flex items-center flex-col justify-center ">
+
+        <h1 className='text-3xl my-3'>Password Hide Show</h1>
+
+        <div className={`input-box relative flex w-[350px] p-2 rounded-[5px] bg-white text-black border-2 ${getBorderColor()} `}>
+          <input type={pshow ? 'text' : 'password'} className='w-full border-0 outline-none text-xl font-medium' placeholder='Enter Password' value={password} onChange={passwordShow} />
+
+          <div className="icon cursor-pointer text-black">
+            <i className={pshow ? "ri-eye-off-fill text-2xl" : "ri-eye-fill text-2xl"} onClick={() => setPshow(!pshow)}></i>
+          </div>
+        {password.length > 0 &&(
+          <p className={`absolute bottom-[-30px] left-0 font-medium ${getTextColor()}`}>Password is {pcheck} </p>
+        )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App
